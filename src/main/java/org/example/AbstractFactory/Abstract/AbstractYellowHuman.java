28ab6ac10017bb5b9b6770da77833334f68f4a9f
miaomiaole <tom12510@164.com>
2021-10-20 19:36:32 +0800

    抽象工厂模式

--- src/main/java/org/example/AbstractFactory/Abstract/AbstractYellowHuman.java
+++ src/main/java/org/example/AbstractFactory/Abstract/AbstractYellowHuman.java
@@ -2,6 +2,9 @@ package org.example.AbstractFactory.Abstract;
 
 import org.example.AbstractFactory.Interface.Human;
 
+/**
+ * 抽象实现类 易于扩展
+ */
 public abstract class AbstractYellowHuman implements Human {
     @Override
     public void getColor() {
