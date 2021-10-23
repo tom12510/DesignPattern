a11c1c384e946e899fc1ed274b62dfd4d8b3a9b1
miaomiaole <tom12510@163.com>
2021-10-24 01:22:06 +0800

    接口隔离原则

---
+++ src/main/java/org/example/Liskov/Liskov.java
@@ -0,0 +1,32 @@
+package org.example.Liskov;
+
+/**
+ * 解决方案
+ * 创建更继承的基类
+ */
+class Base {
+  // 把基础类方法和成员放到Base类
+}
+
+
+class Liskov_A {
+    public int func1(int a, int b) {
+        return a - b;
+    }
+}
+/**
+ *  子类重写父类方法 违反里氏原则
+ *  使其原有逻辑发生改变
+ */
+class Liskov_B extends Liskov_A {
+
+    @Override
+    public int func1(int a, int b) {
+        return a + b;
+    }
+
+    public int func2(int a, int b) {
+        return func1(a, b) + 9;
+    }
+}
+
