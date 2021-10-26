7348bcba803f78a755505381ea327fe759ba30fd
miaomiaole <tom12510@164.com>
2021-10-26 08:58:52 +0800

    建造模式案例

---
+++ src/main/java/org/example/Builder/Builder/ComputerBuilder.java
@@ -0,0 +1,23 @@
+package org.example.Builder.Builder;
+
+import org.example.Builder.Product.Computer;
+
+/**
+ * 抽象构者类(约束产品相关特性),由它来组件各种产品类型的电脑
+ * <p>
+ * (子类具体实现)
+ */
+public abstract class ComputerBuilder {
+
+    public abstract void setUsbCount();
+
+    public abstract void setKeyboard();
+
+    public abstract void setDisplay();
+
+
+
+
+    //产品组装后获取类
+    public abstract Computer getComputer();
+}
---
+++ src/main/java/org/example/Builder/Product/Computer.java
@@ -0,0 +1,73 @@
+package org.example.Builder.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 产品类
+ * 通过参数不同构建不同类型产品
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Computer {
+    private String cpu;//必须
+    private String ram;//必须
+    private String usbCount;//可选
+    private String keyboard;//可选
+    private String display;//可选
+
+    public Computer(String cpu, String ram) {
+        this.cpu = cpu;
+        this.ram = ram;
+    }
+
+    /**
+     * 使用Builder构造类
+     *
+     * @param builder
+     */
+    private Computer(Builder builder) {
+        this.cpu = builder.cpu;
+        this.ram = builder.ram;
+        this.usbCount = builder.usbCount;
+        this.keyboard = builder.keyboard;
+        this.display = builder.display;
+    }
+
+    /**
+     *
+     */
+    public static class Builder {
+        private String cpu;//必须
+        private String ram;//必须
+        private String usbCount;//可选
+        private String keyboard;//可选
+        private String display;//可选
+
+        public Builder(String cup, String ram) {
+            this.cpu = cup;
+            this.ram = ram;
+        }
+
+        public Builder setUsbCount(String usbCount) {
+            this.usbCount = usbCount;
+            return this;
+        }
+
+        public Builder setKeyboard(String keyboard) {
+            this.keyboard = keyboard;
+            return this;
+        }
+
+        public Builder setDisplay(String display) {
+            this.display = display;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+    }
+}
