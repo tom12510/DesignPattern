f4ac4fb2b6badbfc1aeb2c79cae9fdd9a9c392fb
miaomiaole <tom12510@164.com>
2021-10-26 11:32:38 +0800

    建造模式案例

--- src/main/java/org/example/Builder/ConcreteBuilder/MacComputerBuilder.java
+++ src/main/java/org/example/Builder/ConcreteBuilder/MacComputerBuilder.java
@@ -13,22 +13,18 @@ public class MacComputerBuilder extends ComputerBuilder {
     public MacComputerBuilder(String cpu, String ram) {
         computer = new Computer(cpu, ram);
     }
-
     @Override
     public void setUsbCount() {
         computer.setUsbCount("2个雷电接口");
     }
-
     @Override
     public void setKeyboard() {
         computer.setKeyboard("苹果键盘");
     }
-
     @Override
     public void setDisplay() {
         computer.setDisplay("原彩显示屏");
     }
-
     @Override
     public Computer getComputer() {
         return computer;
